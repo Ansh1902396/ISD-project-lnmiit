7b79780c87a32afca4f4058ac9cbe840eb134bc4
Saket2766 <saketsamarth27@gmail.com>
2022-11-27 00:14:28 +0530

    modified:   project/src/Login.js

--- project/src/Login.js
+++ project/src/Login.js
@@ -9,16 +9,24 @@ const Login = () => {
         width: 280,
         margin: "25px auto"
     }
+
+    const btStyle = {
+        margin: '8px 0'
+    }
+
+    const titleStyle = {
+        margin : '8px'
+    }
     
     return(
         <Grid align ='center'>
             <Paper elevation = {10} style = {paperStyle} >
-                <Typography variant='h3'>Login</Typography>
+                <Typography variant='h3'style={titleStyle}>Login</Typography>
 
                 <TextField id = "uField" name = "uField" variant ="outlined" label='Username'  fullWidth required/>
                 <TextField id = "pField" name = "pField" variant ="outlined" label='Password'  type='password' fullWidth required/>
 
-                <Button id = "subButton" name = "subButton" type = 'submit' variant = 'contained'>
+                <Button id = "subButton" name = "subButton" type = 'submit' variant = 'contained' style ={btStyle}>
                 <Typography variant='button'>Login</Typography>
                 </Button>
             </Paper>
