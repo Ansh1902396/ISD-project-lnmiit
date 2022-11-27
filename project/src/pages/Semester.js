ba84dad179e182f361dba78f85f37718082dfb54
Saket2766 <saketsamarth27@gmail.com>
2022-11-27 15:18:17 +0530

    modified:   project/src/pages/Semester.js

--- project/src/pages/Semester.js
+++ project/src/pages/Semester.js
@@ -17,14 +17,22 @@ const Semester = () => {
                 <Grid item xs = {12}>
                     <Paper>
                         <Typography variant="h5" sx = {{padding : 3}}>Current Semester:</Typography><br/>
-                        <Box sx={{padding : 3}}>
+                        <Box sx={{padding: 3}}>
                             <Typography name ="currentSemSubjects" variant ="paragraph">
-                                Mathematics-1 , Technical Communication in English, 
+                                Mathematics-1 , Technical Communication in English, Physics - 1, Computer Programming 
                             </Typography>
                         </Box>
                     </Paper>
                 </Grid>
+
+                <Grid item xs = {12}>
+                    <Paper>
+                        <Typography variant="h5" sx = {{padding : 3}}>Previous Semester:</Typography><br/>
+                        
+                    </Paper>
+                </Grid>
             </Grid>
+
         </Container>
     );
     
