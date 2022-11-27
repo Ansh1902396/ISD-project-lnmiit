abdf6990c87c385f544e878183813c44df375c24
Saket2766 <saketsamarth27@gmail.com>
2022-11-27 12:11:39 +0530

    modified:   project/public/index.html 	modified:   project/src/App.js 	modified:   project/src/pages/Home.js

--- project/src/App.js
+++ project/src/App.js
@@ -4,19 +4,30 @@ import Login from './pages/Login';
 import Home from './pages/Home';
 import Navbar from './Navbar';
 import {Route, Routes} from "react-router-dom";
+import {createTheme, ThemeProvider} from '@mui/material'; 
+import { Box } from '@mui/material';
 function App(){
-  return (
-    <div className="App">
-      
-      <Navbar/>
 
-      <Routes>
-        <Route path = "/" element = {<Home/>}></Route>
-        <Route path = "/Login" element = {<Login/>}></Route>
-      </Routes>
+  const appTheme = createTheme({
+    palette: {
+      mode : "light",
+    }
+  });
+
+  return (
+    <ThemeProvider theme={appTheme}>
+      <Box>
+        
+        <Navbar/>
+
+        <Routes>
+          <Route path = "/" element = {<Home/>}></Route>
+          <Route path = "/Login" element = {<Login/>}></Route>
+        </Routes>
+        
       
-      
-    </div>
+      </Box>
+    </ThemeProvider>
   );
 }
 
