48ef96013e6f657427fb1f4bd649af58954b2823
Saket2766 <saketsamarth27@gmail.com>
2022-11-27 10:31:27 +0530

    modified:   project/src/App.js

--- project/src/App.js
+++ project/src/App.js
@@ -9,6 +9,7 @@ function App(){
     <div className="App">
       
       <Navbar/>
+
       <Routes>
         <Route path = "/" element = {Home}></Route>
         <Route path = "/Login" element = {Login}></Route>
