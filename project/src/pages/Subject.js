4a20f973a756c27767fe9f35b74303d841994068
Saket2766 <saketsamarth27@gmail.com>
2022-11-27 18:47:14 +0530

    modified:   project/src/pages/Subject.js

--- project/src/pages/Subject.js
+++ project/src/pages/Subject.js
@@ -3,6 +3,10 @@ import { Grid,Box } from '@mui/material';
 const Subject = () => {
     return (
         <Grid container >
+
+            <Box item xs = {4}>
+                
+            </Box>
             
         </Grid>
     );
