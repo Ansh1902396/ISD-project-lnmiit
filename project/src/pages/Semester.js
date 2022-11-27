1cf840d96385a39f4e3ef33cf333cf51d0d3eeae
Saket2766 <saketsamarth27@gmail.com>
2022-11-27 14:22:20 +0530

    modified:   project/src/pages/Semester.js

--- project/src/pages/Semester.js
+++ project/src/pages/Semester.js
@@ -11,11 +11,13 @@ const Semester = () => {
             <Grid container spacing={3}>
                 <Grid item xs = {12}>
                     <Paper>
-                        <Typography variant="h4" sx = {{padding : 3}}> Name: {name}</Typography>
+                        <Typography variant="h5" sx = {{padding : 3}}> Name: {name}</Typography>
                     </Paper>
                 </Grid>
                 <Grid item xs = {12}>
-                    
+                    <Paper>
+                        <Typography variant="h5" sx = {{padding : 3}}>Current Semester:</Typography>
+                    </Paper>
                 </Grid>
             </Grid>`
         </Container>
